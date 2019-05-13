3341953741a41f96625f50c5cbb7c3e453beece2
sve-axelor <s.vekaria@axelor.com>
2019-05-13 11:24:28 +0530

    Add project on git
    Signed-off-by: sve-axelor <s.vekaria@axelor.com>

---
+++ MyFirstTry/src/main/java/Demo/Add.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Evey one the first try is  done success.....@@@@");
+
+	}
+
+}
